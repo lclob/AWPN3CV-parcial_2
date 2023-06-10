45792e74d68edc90af9f104bdeabe498b2c329b4
lclob <116601056+lclob@users.noreply.github.com>
2023-06-09 22:27:39 -0300

    refactor: tmdb api for movies trailer

--- js/main.js
+++ js/main.js
@@ -4,7 +4,7 @@ const buttonSearch = document.getElementById('buscar');
 const resultElement = document.getElementById('resultado');
 const APIkey = '7f2dd986';
 const APIGoogle = 'AIzaSyAYW3g3NRld4PtVL4Bmz04tueXbASg8o-g';
-const APIYoutube = 'AIzaSyBtw6tawmeSBff5hi10gg3w-YZXVbWETUI'
+const APItmbdb = '3fae9e615d9aaca0a304824bd82082e1'
 let page = 1;
 let favs = [];
 
@@ -71,21 +71,22 @@ function btnKey() {
 
 // crea un spinner hasta que llega la respuesta de la API
 function spinner(resultado) {
-  resultado.innerHTML = '';
-  let spinner = document.createElement('div');
-  spinner.classList.add('d-flex', 'justify-content-center', 'spinner', 'w-100');
-  spinner.innerHTML = `
-    <div class="spinner-border" role="status">
-      <span class="visually-hidden">Loading...</span>
-    </div>
-  `;
-  resultado.append(spinner);
+  if(resultado){
+    resultado.innerHTML = '';
+    let spinner = document.createElement('div');
+    spinner.classList.add('d-flex', 'justify-content-center', 'spinner', 'w-100');
+    spinner.innerHTML = `
+      <div class="spinner-border" role="status">
+        <span class="visually-hidden">Loading...</span>
+      </div>
+    `;
+    resultado.append(spinner);
+  }
 };
 
 // crea una card con los datos de la busqueda
 function setData(data) {
   data.Search.forEach(movie => {
-    console.log(movie)
     let div = document.createElement("div");
     div.classList.add('col');
     resultElement.appendChild(div);
@@ -146,61 +147,85 @@ function modalMovieDetails(data, div) {
       <img src="${data.Poster}" alt="Imagen de ${data.Title}" class="details-image" />
       <div class="px-md-3"> 
         <h3 id="staticBackdropLabel">${data.Title}</h3>
-        <small class="d-block text-secondary mt-2">${data.Genre}</small>
         <small class="d-block mt-1">${data.Country}</small>
+        <small class="d-block text-secondary">${data.Genre}</small>
         <span class="badge bg-primary rounded-pill mt-2 rating">${data.imdbRating}</span>
       </div>
       <button type="button" class="btn-close bg-light" data-bs-dismiss="modal" aria-label="Close"></button>
     </div>
     <div class="modal-body">
       <p>${data.Plot}</p>
-      <ul class="list-group table-striped">
+      <ul class="list-group table-striped mb-2">
         <li class="list-group-item fw-semibold">Reparto: <span class="fw-normal">${data.Actors}</span></li>
         <li class="list-group-item fw-semibold">Director: <span class="fw-normal">${data.Director}</span></li>
         <li class="list-group-item fw-semibold">Escritor: <span class="fw-normal">${data.Writer}</span></li>
         <li class="list-group-item fw-semibold">Duración: <span class="fw-normal">${data.Runtime}</span></li>
         <li class="list-group-item fw-semibold">Lanzamiento: <span class="fw-normal">${data.Released}</span></li>
       </ul>
-      <div id="ytplayer"></div>
+      <div class="trailer video-container-${data.imdbID} mt-1" class="my-1 rounded"></div>
     </div>
     <div class="modal-footer">
       <button type="button" data-id="${data.imdbID}" class="btn btn-primary w-100 btnFav">Agregar a favoritos</button>
     </div>
   `;
 
-  let btnFav = content.querySelector('.btnFav')
-  agregarFavoritos(data.imdbID, btnFav);
-  console.log(data)
+  MovieTrailer(`${data.Title}`, `${data.imdbID}`);
 }
 
-//favoritos
-function actualizarFavoritos() {
-  return JSON.parse(localStorage.getItem("favoritos")) || null;
-}
-
-function agregarFavoritos(data, btn) {
-  btn.addEventListener('click', function () {
-    let favoritos = actualizarFavoritos();
-    if (!favoritos) {
-      favs.push(data);
-      localStorage.setItem("favoritos", JSON.stringify([favs]));
-    } else{
-        let favMovie = favs.filter(movie => movie == data)[0];
-        if(!favMovie){
-          favs.push(data);
-          localStorage.setItem("favoritos", JSON.stringify([favs]));
-        }
+// trailer
+function MovieTrailer(movie, id) {
+  const options = {
+    method: 'GET',
+    headers: {
+      accept: 'application/json',
+      Authorization: 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiIzZmFlOWU2MTVkOWFhY2EwYTMwNDgyNGJkODIwODJlMSIsInN1YiI6IjY0ODNhZDY1YmYzMWYyNTA1NzA1YjgwZiIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.YM-U9dI8Sffx6sY9_X4J3r_WYGHrrOcQjY-8KJy2vcE'
     }
-    console.log(favs)
-    btn.innerText = 'Agregado a favoritos'
-  })
+  };
+  
+  // API TMDB
+  fetch(`https://api.themoviedb.org/3/search/movie?query=${movie}&language=en-US&page=1`, options)
+    .then(response => response.json())
+    .then(response => {
+      let videoID = response.results[0]?.id;
+
+      // llamo a la api por id de pelicula para conseguir el id del video
+      if(videoID){
+        fetch(`https://api.themoviedb.org/3/movie/${response.results[0].id}/videos?language=en-US`, options)
+          .then(response => response.json())
+          .then(response => {
+            let videoKEY = response.results[0]?.key;
+            console.log(videoKEY)
+
+            if(videoKEY){
+              let videoContainer = document.querySelector(`.video-container-${id}`);
+              spinner(videoContainer);
+
+            // Crear elemento de iframe para mostrar el video de YouTube
+              let iframeElement = document.createElement('iframe');
+              iframeElement.src = `https://www.youtube.com/embed/${videoKEY}`;
+              iframeElement.allowFullscreen = true;
+              iframeElement.width = '100%';
+              iframeElement.height = '100%';
+              
+              // Limpiar cualquier contenido previo dentro del contenedor
+              videoContainer.innerHTML = `
+                <span class="h5 mt-4 mb-2 d-block">Trailer</span>
+              `;
+              videoContainer.appendChild(iframeElement);
+            }
+          })
+          .catch(err => console.error(err));
+      }
+    })
+    .catch(err => console.error(err));
 }
 
-// salvavidas
+
+//salvavidas
 function salvaVidas() {
-  resultElement.innerHTML = "<p>Lo siento, no hemos encontrado resultados de su busqueda.</p>"
+  resultElement.innerHTML = "<p>Lo sentimos, no hemos encontrado los resultados de su busqueda.</p>"
 }
 
-// llamado funciones
+//funciones
 btn();
 btnKey();
